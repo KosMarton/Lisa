07eeb56cecdafb0108b2ec01d5bf98d647dc71a6
Kós Márton <kosmarci73@gmail.com>
2025-12-30 13:45:07 +0100

    Added static Input class

--- Lisa/src/Lisa/Window.h
+++ Lisa/src/Lisa/Window.h
@@ -39,6 +39,8 @@ namespace Lisa {
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
 
+		virtual void* GetNativeWindow() const = 0;
+
 		static Window* Create(const WindowProps& props = WindowProps()); // Gets implemented per platform
 	};
 
