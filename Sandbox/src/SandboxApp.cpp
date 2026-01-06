ea8254f61bda36e0564d34c84c30b6fa3691eb0a
Kós Márton <kosmarci73@gmail.com>
2026-01-06 21:01:43 +0100

    Added shader library

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -92,7 +92,7 @@ public:
 			}
 		)";
 
-		m_Shader.reset(Lisa::Shader::Create(vertexSrc, fragmentSrc));
+		m_Shader = Lisa::Shader::Create("VertexPosColor", vertexSrc, fragmentSrc);
 
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
@@ -126,48 +126,15 @@ public:
 			}
 		)";
 
-		m_FlatColorShader.reset(Lisa::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+		m_FlatColorShader = Lisa::Shader::Create("FlatColorShader", flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;			
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-
-			uniform sampler2D u_Texture;
-			
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(Lisa::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
 
 		m_Texture = Lisa::Texture2D::Create("assets/textures/Checkerboard.png");
 		m_BlendTexture = Lisa::Texture2D::Create("assets/textures/DababyCar.png");
 
-		std::dynamic_pointer_cast<Lisa::OpenGLShader>(m_TextureShader)->Bind();
-		std::dynamic_pointer_cast<Lisa::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
+		std::dynamic_pointer_cast<Lisa::OpenGLShader>(textureShader)->Bind();
+		std::dynamic_pointer_cast<Lisa::OpenGLShader>(textureShader)->UploadUniformInt("u_Texture", 0);
 	}
 
 	void OnUpdate(Lisa::Timestep ts) override
@@ -210,10 +177,12 @@ public:
 			}
 		}
 
+		auto textureShader = m_ShaderLibrary.Get("Texture");
+
 		m_Texture->Bind();
-		Lisa::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		Lisa::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 		m_BlendTexture->Bind();
-		Lisa::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		Lisa::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		// Triangle
 		// Lisa::Renderer::Submit(m_Shader, m_VertexArray);
@@ -232,10 +201,11 @@ public:
 	{
 	}
 private:
+	Lisa::ShaderLibrary m_ShaderLibrary;
 	Lisa::Ref<Lisa::Shader> m_Shader;
 	Lisa::Ref<Lisa::VertexArray> m_VertexArray;
 
-	Lisa::Ref<Lisa::Shader> m_FlatColorShader, m_TextureShader;
+	Lisa::Ref<Lisa::Shader> m_FlatColorShader;
 	Lisa::Ref<Lisa::VertexArray> m_SquareVA;
 
 	Lisa::Ref<Lisa::Texture2D> m_Texture, m_BlendTexture;
