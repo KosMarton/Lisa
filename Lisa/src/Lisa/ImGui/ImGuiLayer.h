de031f50c81c0122b2769513737c62563d504ea1
Kós Márton <kosmarci73@gmail.com>
2026-01-24 12:38:15 +0100

    Made the editor look good, added ImGui fonts and styles

--- Lisa/src/Lisa/ImGui/ImGuiLayer.h
+++ Lisa/src/Lisa/ImGui/ImGuiLayer.h
@@ -22,6 +22,8 @@ namespace Lisa {
 		void End();
 
 		void BlockEvents(bool block) { m_BlockEvents = block; }
+
+		void SetDarkThemeColors();
 	private:
 		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
