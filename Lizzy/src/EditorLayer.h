8dab3e6480382e6e67787507dc0ef34db2ab924e
Kós Márton <kosmarci73@gmail.com>
2026-01-25 16:09:54 +0100

    Open and save scenes with file dialoge + shortcuts

--- Lizzy/src/EditorLayer.h
+++ Lizzy/src/EditorLayer.h
@@ -18,6 +18,12 @@ namespace Lisa {
 		virtual void OnImGuiRender() override;
 		void OnEvent(Event& e) override;
 	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
+
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
+	private:
 		Lisa::OrthographicCameraController m_CameraController;
 
 		// Temp
