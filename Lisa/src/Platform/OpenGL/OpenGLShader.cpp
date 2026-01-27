3c7cc9d92a24f37ced9002dd8b10a223151c3453
Kós Márton <kosmarci73@gmail.com>
2026-01-27 20:08:16 +0100

    Added missing SetFloat2 function to renderer + minor code changes

--- Lisa/src/Platform/OpenGL/OpenGLShader.cpp
+++ Lisa/src/Platform/OpenGL/OpenGLShader.cpp
@@ -216,6 +216,13 @@ namespace Lisa {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		LS_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		LS_PROFILE_FUNCTION();
