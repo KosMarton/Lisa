40156b522401b9546fa4bcf43ed4db1d752d9c68
Kós Márton <kosmarci73@gmail.com>
2026-01-27 19:54:36 +0100

    Clearing Framebuffer texture attachment with API function

--- Lisa/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ Lisa/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -76,6 +76,18 @@ namespace Lisa {
 			return false;
 		}
 
+		static GLenum LisaFBTextureFormatToGL(FramebufferTextureFormat format)
+		{
+			switch (format)
+			{
+				case FramebufferTextureFormat::RGBA8:       return GL_RGBA8;
+				case FramebufferTextureFormat::RED_INTEGER: return GL_RED_INTEGER;
+			}
+
+			LS_CORE_ASSERT(false);
+			return 0;
+		}
+
 	}
 
 	OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& spec)
@@ -199,6 +211,15 @@ namespace Lisa {
 		glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
 		return pixelData;
 
+	}
+
+	void OpenGLFramebuffer::ClearAttachment(uint32_t attachmentIndex, int value)
+	{
+		LS_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+
+		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
+		glClearTexImage(m_ColorAttachments[attachmentIndex], 0,
+			Utils::LisaFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
 	}
 
 }
