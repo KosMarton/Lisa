e17305c95a650b355a7d360e86ffd6adeb93b395
Kós Márton <kosmarci73@gmail.com>
2026-01-31 17:16:22 +0100

    Drag drop textures to entities, with tint and tiling

--- Lisa/src/Lisa/Scene/Components.h
+++ Lisa/src/Lisa/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "Lisa/Scene/SceneCamera.h"
 #include "Lisa/Scene/ScriptableEntity.h"
+#include "Lisa/Renderer/Texture.h"
 
 namespace Lisa {
 
@@ -45,6 +46,8 @@ namespace Lisa {
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
