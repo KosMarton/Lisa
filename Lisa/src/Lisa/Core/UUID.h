da73b231e34b4970201913768da865e3e03b33bf
Kós Márton <kosmarci73@gmail.com>
2026-02-05 17:24:39 +0100

    Simplified UUID hash specialization

--- Lisa/src/Lisa/Core/UUID.h
+++ Lisa/src/Lisa/Core/UUID.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <xhash>
-
 namespace Lisa {
 
 	class UUID
@@ -19,13 +17,14 @@ namespace Lisa {
 }
 
 namespace std {
+	template<typename T> struct hash;
 
 	template<>
 	struct hash<Lisa::UUID>
 	{
 		std::size_t operator()(const Lisa::UUID& uuid) const
 		{
-			return hash<uint64_t>()((uint64_t)uuid);
+			return (uint64_t)uuid;
 		}
 	};
 
