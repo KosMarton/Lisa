3317254569d01ebeaeb72b412dd6312fd2c7a7b9
Kós Márton <kosmarci73@gmail.com>
2026-02-07 20:26:33 +0100

    Calling C++ code from C# and more functionality - Basically everything needed for scripting

--- Lisa-ScriptCore/Source/Main.cs
+++ Lisa-ScriptCore/Source/Main.cs
@@ -1,16 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Lisa
 {
 
+    public struct Vector3
+    {
+        public float X, Y, Z;
+
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
     public class Main
     {
-
         public float FloatVar { get; set; }
 
         public Main()
         {
             Console.WriteLine("Main constructor!");
+            Log("Lisa", 8058);
+
+            Vector3 pos = new Vector3(5, 2.5f, 1);
+            Vector3 result = Log(pos);
+            Console.WriteLine($"{result.X}, {result.Y}, {result.Z}");
+            Console.WriteLine("{0}", NativeLog_VectorDot(ref pos));
         }
 
         public void PrintMessage()
@@ -33,6 +51,25 @@ namespace Lisa
             Console.WriteLine($"C# says: {message}");
         }
 
+        private void Log(string text, int parameter)
+        {
+            NativeLog(text, parameter);
+        }
+
+        private Vector3 Log(Vector3 parameter)
+        {
+            NativeLog_Vector(ref parameter, out Vector3 result);
+            return result;
+        }
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void NativeLog(string text, int parameter);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static void NativeLog_Vector(ref Vector3 parameter, out Vector3 result);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern static float NativeLog_VectorDot(ref Vector3 parameter);
     }
 
 }
--- Lisa/src/Lisa/Scripting/ScriptEngine.cpp
+++ Lisa/src/Lisa/Scripting/ScriptEngine.cpp
@@ -101,6 +101,26 @@ namespace Lisa {
 		}
 	}
 
+	static void NativeLog(MonoString* string, int parameter)
+	{
+		char* cStr = mono_string_to_utf8(string);
+		std::string str(cStr);
+		mono_free(cStr);
+		std::cout << str << ", " << parameter << std::endl;
+	}
+
+	static void NativeLog_Vector(glm::vec3* parameter, glm::vec3* outResult)
+	{
+		//LS_CORE_WARN("Value: {0}", *parameter);
+		*outResult = glm::normalize(*parameter);
+	}
+
+	static float NativeLog_VectorDot(glm::vec3* parameter)
+	{
+		//LS_CORE_WARN("Value: {0}", *parameter);
+		return glm::dot(*parameter, *parameter);
+	}
+
 	void ScriptEngine::InitMono()
 	{
 		mono_set_assemblies_path("mono/lib");
@@ -114,6 +134,10 @@ namespace Lisa {
 		// Create an App Domain
 		s_Data->AppDomain = mono_domain_create_appdomain("LisaScriptRuntime", nullptr);
 		mono_domain_set(s_Data->AppDomain, true);
+
+		mono_add_internal_call("Lisa.Main::NativeLog", NativeLog);
+		mono_add_internal_call("Lisa.Main::NativeLog_Vector", NativeLog_Vector);
+		mono_add_internal_call("Lisa.Main::NativeLog_VectorDot", NativeLog_VectorDot);
 
 		// Move this maybe
 		s_Data->CoreAssembly = LoadCSharpAssembly("Resources/Scripts/Lisa-ScriptCore.dll");
