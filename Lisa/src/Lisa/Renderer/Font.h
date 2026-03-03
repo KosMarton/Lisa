86d6a16cfd84e98799f06eaa35baa3ccd6d9a143
Kós Márton <kosmarci73@gmail.com>
2026-03-03 23:22:24 +0100

    Created MSDF texture atlas, and improved Texture API - added TextureSpecification struct

--- Lisa/src/Lisa/Renderer/Font.h
+++ Lisa/src/Lisa/Renderer/Font.h
@@ -1,13 +1,23 @@
 #pragma once
 
+#include "Lisa/Renderer/Texture.h"
+
+#include <filesystem>
+
 namespace Lisa {
+
+	struct MSDFData;
 
 	class Font
 	{
 	public:
 		Font(const std::filesystem::path& filePath);
+		~Font();
 
-
+		Ref<Texture2D> GetAtlasTexture() const { return m_AtlasTexture; }
+	private:
+		MSDFData* m_Data;
+		Ref<Texture2D> m_AtlasTexture;
 	};
 
 }
--- Lisa/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Lisa/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -5,13 +5,41 @@
 
 namespace Lisa {
 
-	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
-		: m_Width(width), m_Height(height)
+	namespace Utils {
+
+		static GLenum LisaImageFormatToGLDataFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB8:   return GL_RGB;
+				case ImageFormat::RGBA8: return GL_RGBA;
+			}
+
+			LS_CORE_ASSERT(false);
+			return 0;
+		}
+
+		static GLenum LisaImageFormatToGLInternalFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB8:   return GL_RGB8;
+				case ImageFormat::RGBA8: return GL_RGBA8;
+			}
+
+			LS_CORE_ASSERT(false);
+			return 0;
+		}
+
+	}
+
+	OpenGLTexture2D::OpenGLTexture2D(const TextureSpecification& specification)
+		: m_Specification(specification), m_Width(specification.Width), m_Height(specification.Height)
 	{
 		LS_PROFILE_FUNCTION();
 
-		m_InternalFormat = GL_RGBA8;
-		m_DataFormat = GL_RGBA;
+		m_InternalFormat = Utils::LisaImageFormatToGLInternalFormat(m_Specification.Format);
+		m_DataFormat = Utils::LisaImageFormatToGLDataFormat(m_Specification.Format);
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
 		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
